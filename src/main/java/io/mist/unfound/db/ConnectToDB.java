13ce69e6c7a88ff51237d27d3164c1e9698204cc
Daniel <daniel.corder@asu.edu>
2021-01-27 21:43:02 -0500

    Added a new class to connect to a SQLite database and updated the gradle build file with the dependency information; the class is basic and yet to be much expanded.

---
+++ src/main/java/io/mist/unfound/db/ConnectToDB.java
@@ -0,0 +1,39 @@
+package io.mist.unfound.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectToDB {
+    /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/sqlite/chinook.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+    }
+}
